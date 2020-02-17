894240961f2b1797a9fc87c1e90e2c2817d0e0ac
xuanweishan <xuanweishan@gamil.com>
2020-02-17 15:05:47 +0800

    新增加法與減法

--- 9cc.c
+++ 9cc.c
@@ -2,15 +2,33 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv){
-	if (argc !=2){
+	if (argc != 2){
 		fprintf(stderr, "引用數數量錯誤");
 		return 1;
 	}
 
+	char *p = argv[1];
+
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
-	printf("	mov rax, %d\n", atoi(argv[1]));
+	printf("	mov rax, %ld\n", strtol(p, &p, 10));
+
+	while(*p){
+		if(*p == '+'){
+			p++;
+			printf("	add rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+		if(*p == '-'){
+			p++;
+			printf("	sub rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		fprintf(stderr, "預料之外的文字: '%c'", *p);
+	}
+
 	printf("	ret\n");
 	return 0;
 }
