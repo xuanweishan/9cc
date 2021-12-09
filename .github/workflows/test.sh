d5216cde8f528e5bf95b099caa2bf986859f41f8
xuanweishan <xuanweishan@gmail.com>
2021-12-09 14:03:28 +0800

    Add test.sh to workflow directory

---
+++ .github/workflows/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+try(){
+	expected="$1"
+	input="$2"
+
+	./9cc "$input" > tmp.s
+	gcc -o tmp tmp.s
+	./tmp
+	actual="$?"
+
+	if [ "$actual" = "$expected" ]; then
+		echo "$input => $actual"
+	else
+		echo "$input => $expected expected, but got $actual"
+		exit 1
+	fi
+}
+
+try 0 0
+try 42 42
+try 21 "5+20-4"
+try 41 " 12 + 34 - 5 "
+
+echo OK
+echo test pass
+echo test for test
